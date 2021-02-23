fdc9f5b6e18bbdb617d3deba45718427dbd7ea21
Duda Duarte <dudammduarte@yahoo.com.br>
2021-02-23 00:32:49 -0300

    first commit - create first route, controller and test in elixir

---
+++ test/rocketpay/numbers_test.exs
@@ -0,0 +1,19 @@
+defmodule Rocketpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Rocketpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "when there is a file with the given name, return the sum of the numbers" do
+    response = Numbers.sum_from_file("numbers")
+    expected_response = {:ok, %{result: 37}}
+    assert response == expected_response
+    end
+
+    test "when there is no file with the given name, return an error" do
+    response = Numbers.sum_from_file("banana")
+    expected_response = {:error, %{message: "Invalid File."}}
+    assert response == expected_response
+    end
+  end
+end
